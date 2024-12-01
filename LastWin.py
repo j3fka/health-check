38b58d700a02ed5d578124e96bf9b38dc1d8747a
j3fka <sodmiva@gmail.com>
2024-12-01 11:34:41 +0700

    1.3.3

--- LastWin.py
+++ LastWin.py
@@ -3,12 +3,16 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from instr import *
 
 class LastWin(QWidget):
-    def __init__(self):
+    def __init__(self, age, result_1, result_2, result_3):
         super().__init__()
         self.set_appear()
         self.initUI()
         self.show()
 
+        self.age = age
+        self.result_1 = result_1
+        self.result_2 = result_2
+        self.result_3 = result_3
     def set_appear(self):
         self.resize(win_width, win_height)
         self.setWindowTitle(txt_title)
--- SecondWin.py
+++ SecondWin.py
@@ -1,5 +1,6 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QFont
 from instr import *
 from LastWin import LastWin
 
@@ -26,10 +27,10 @@ class SecondWin(QWidget):
         self.txt_hinttest1 = QLineEdit(txt_hinttest1)
         self.txt_test2 = QLabel(txt_test2)
         self.txt_starttest2 = QPushButton(txt_starttest2)
-        self.txt_hinttest2 = QLineEdit(txt_hinttest2)
         self.txt_test3 = QLabel(txt_test3)
         self.txt_starttest3 = QPushButton(txt_starttest3)
-        self.txt_hinttest3 = QLabel(txt_hinttest3)
+        self.txt_hinttest2 = QLineEdit(txt_hinttest2)
+        self.txt_hinttest3 = QLineEdit(txt_hinttest3)
         self.txt_sendresults = QPushButton(txt_sendresults)
 
         self.timer = QLabel('0:00:15')
@@ -47,23 +48,85 @@ class SecondWin(QWidget):
         self.v_layout_1.addWidget(self.txt_hinttest1, alignment=Qt.AlignLeft)
         self.v_layout_1.addWidget(self.txt_test2, alignment=Qt.AlignLeft)
         self.v_layout_1.addWidget(self.txt_starttest2, alignment=Qt.AlignLeft)
-        self.v_layout_1.addWidget(self.txt_hinttest2, alignment=Qt.AlignLeft)
         self.v_layout_1.addWidget(self.txt_test3, alignment=Qt.AlignLeft)
         self.v_layout_1.addWidget(self.txt_starttest3, alignment=Qt.AlignLeft)
+        self.v_layout_1.addWidget(self.txt_hinttest2, alignment=Qt.AlignLeft)
         self.v_layout_1.addWidget(self.txt_hinttest3, alignment=Qt.AlignLeft)
-        self.v_layout_1.addWidget(self.txt_sendresults, alignment=Qt.AlignLeft)
+        self.v_layout_1.addWidget(self.txt_sendresults, alignment=Qt.AlignCenter)
 
         self.v_layout_2.addWidget(self.timer, alignment=Qt.AlignCenter)
+        self.timer.hide()
 
         self.h_layout_1.addLayout(self.v_layout_1)
         self.h_layout_1.addLayout(self.v_layout_2)
     
 
         self.setLayout(self.h_layout_1)
-        
+
+    def test_1(self):
+        self.timer.show()
+        global time
+        time = QTime(0, 0, 15)
+        self.timer.setText(time.toString('hh:mm:ss'))
+        self.timer.setFont(QFont('Times', 36, QFont.Bold))
+        self.timer.setStyleSheet('color: black')
+        self.timer_for_test_1 = QTimer()
+        self.timer_for_test_1.timeout.connect(self.tick_1)
+        self.timer_for_test_1.start(1000)
+
+    def tick_1(self):
+        global time
+        time = time.addSecs(-1)
+        self.timer.setText(time.toString('hh:mm:ss'))
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer_for_test_1.stop()
+
+    def test_2(self):
+        self.timer.show()
+        global time
+        time = QTime(0, 0, 30)
+        self.timer.setText(time.toString('hh:mm:ss')[-2:])
+        self.timer.setFont(QFont('Times', 36, QFont.Bold))
+        self.timer.setStyleSheet('color: black')
+        self.timer_for_test_2 = QTimer()
+        self.timer_for_test_2.timeout.connect(self.tick_2)
+        self.timer_for_test_2.start(1500)
+    
+    def tick_2(self):
+        global time
+        time = time.addSecs(-1)
+        self.timer.setText(time.toString('hh:mm:ss')[-2:])
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer_for_test_2.stop()
+
+    def test_3(self):
+        self.timer.show()
+        global time
+        time = QTime(0, 1, 0)
+        self.timer.setText(time.toString('hh:mm:ss'))
+        self.timer.setFont(QFont('Times', 36, QFont.Bold))
+        self.timer.setStyleSheet('color: green')
+        self.timer_for_test_3 = QTimer()
+        self.timer_for_test_3.timeout.connect(self.tick_3)
+        self.timer_for_test_3.start(1000)
+
+    def tick_3(self):
+        global time
+        time = time.addSecs(-1)
+        self.timer.setText(time.toString('hh:mm:ss'))
+        if time.toString('hh:mm:ss')[-2:] == '15':
+            self.timer.setStyleSheet('color: green')
+        if time.toString('hh:mm:ss')[-2:] == '45':
+            self.timer.setStyleSheet('color: black')
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer_for_test_3.stop()
+    
+
     def connects(self):
         self.txt_sendresults.clicked.connect(self.nextwin)
+        self.txt_starttest1.clicked.connect(self.test_1)
+        self.txt_starttest2.clicked.connect(self.test_2)
+        self.txt_starttest3.clicked.connect(self.test_3)
     def nextwin(self):
         self.hide()
-        self.lw = LastWin()
-
+        self.lw = LastWin(self.txt_hintage.text(), self.txt_hinttest1.text(), self.txt_hinttest2.text(), self.txt_hinttest3.text())
