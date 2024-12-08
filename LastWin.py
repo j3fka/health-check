ed040f66213958a78b0c98abffba782185c3d6ae
j3fka <sodmiva@gmail.com>
2024-12-08 11:11:29 +0700

    1.4

--- LastWin.py
+++ LastWin.py
@@ -1,21 +1,34 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 from instr import *
+from matan import *
+
 
 class LastWin(QWidget):
     def __init__(self, age, result_1, result_2, result_3):
+        self.age = age
+        self.result_1 = result_1
+        self.result_2 = result_2
+        self.result_3 = result_3
+
         super().__init__()
         self.set_appear()
         self.initUI()
         self.show()
 
-        self.age = age
-        self.result_1 = result_1
-        self.result_2 = result_2
-        self.result_3 = result_3
     def set_appear(self):
         self.resize(win_width, win_height)
         self.setWindowTitle(txt_title)
         self.move(win_x, win_y)
     def initUI(self):
-        pass
+        index_r, be = compare(self.result_1, self.result_2, self.result_3, self.age)
+        self.v_layout = QVBoxLayout()
+        self.index = QLabel(f'Индекс Руфье: {index_r}')
+        self.preferably = QLabel(f'Работоспособность сердца: {be}')
+
+        self.v_layout.addWidget(self.index, alignment = Qt.AlignCenter)
+        self.v_layout.addWidget(self.preferably, alignment = Qt.AlignCenter)
+        self.setLayout(self.v_layout)
+
+
+
--- matan.py
+++ matan.py
@@ -1,9 +1,38 @@
-def index_r(p1, p2, p3):
-    return (4 * (p1+p2+p3) - 200) / 10
+def index_r(p1, p2, p3):                    #индекс
+    return (4 * (int(p1) + int(p2) + int(p3)) - 200) / 10
 
-def compare(index_r(p1, p2, p3), age):
-    a = (min(age, 15) - 7) // 2
-    b = 21 - 1.5 * a
+def low_coast(age):          
+    a = (min(int(age), 15) - 7) // 2             #в каком столбце (4, 3, 2, 1, 0)
+    low = 21 - 1.5 * a
+    return low
 
+def strokes(index, low):
+    if index >= low:
+        return 0
+    if index >= low - 4:
+        return 1
+    if index >= low - 5:
+        return 2
+    if index >= low - 5.5:
+        return 3
+    return 4
 
+def compare(p1, p2, p3, age):
+    if int(age) < 7:
+        return 'не найден', 'невозможно расчитать'
+    index = index_r(p1, p2, p3)
+    low = low_coast(age)
+    stroke = strokes(index, low)
+    return index, txt_res[stroke]
 
+txt_res = []
+txt_res.append('''низкая.
+Срочно обратитесь к врачу!''')
+txt_res.append('''удовлетворительная.
+Обратитесь к врачу!''')
+txt_res.append('''средняя.
+Возможно, стоит дополнительно обследоваться у врача.''')
+txt_res.append('''
+выше среднего''')
+txt_res.append('''
+высокая''')
